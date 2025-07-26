5b474cb4f6acf6731ec1ce84e659b35728b8a1bf
RK Anik <30260735+rkanik@users.noreply.github.com>
2025-07-26 15:14:20 +0600

    Change app name and logo

---
+++ expo/app/(tabs)/explore.tsx
@@ -0,0 +1,10 @@
+import { View } from '@/components/base/View';
+import { Text } from '@/components/base/Text';
+
+export default function TabTwoScreen() {
+  return (
+    <View className="flex-1 bg-base">
+      <Text>Explore</Text>
+    </View>
+  );
+}
