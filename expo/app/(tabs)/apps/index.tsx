1169880d7c77d36ec62690487fdef87a0ee6986b
RK Anik <30260735+rkanik@users.noreply.github.com>
2025-07-26 23:20:27 +0600

    Added packages

--- expo/app/(tabs)/apps/index.tsx
+++ expo/app/(tabs)/apps/index.tsx
@@ -1,10 +1,34 @@
 import { Text } from '@/components/base/Text';
 import { View } from '@/components/base/View';
+import { Link } from 'expo-router';
+
+const apps = [
+  {
+    name: 'Contacts',
+    description: 'App 1 description',
+    icon: '🚀',
+    href: '/apps/contacts',
+  },
+  {
+    name: 'Messages',
+    description: 'App 2 description',
+    icon: '🚀',
+    href: '/apps/messages',
+  },
+];
 
 export default function Screen() {
   return (
-    <View className="flex-1 bg-base">
-      <Text>Apps</Text>
+    <View className="flex-1 bg-base p-4">
+      <View className="flex-1 flex-row flex-wrap gap-4">
+        {apps.map((app) => (
+          <Link href={app.href as any} key={app.name}>
+            <View className="rounded bg-base2 p-4 shadow">
+              <Text>{app.name}</Text>
+            </View>
+          </Link>
+        ))}
+      </View>
     </View>
   );
 }
---
+++ expo/app/apps/contacts/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from '@/components/base/Text';
+import { View } from '@/components/base/View';
+
+export default function Screen() {
+  return (
+    <View className="flex-1 bg-base p-4">
+      <Text>Contacts</Text>
+    </View>
+  );
+}
