3d0bd565965bfa92ef6efa66605e638606fc7a31
RK Anik <30260735+rkanik@users.noreply.github.com>
2025-08-14 14:01:30 +0600

    Add useCreated and useMounted hooks
    Introduces two custom hooks, useCreated and useMounted, for handling component creation and mount lifecycle events. Integrates these hooks into the Home page to log messages on creation and mount.

--- react-base/src/app/page.tsx
+++ react-base/src/app/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+import { useCreated } from "@/hooks/useCreated";
+import { useMounted } from "@/hooks/useMounted";
+
 export default function Home() {
+  //
+  useCreated(() => {
+    console.log("useCreated");
+  });
+
+  useMounted(() => {
+    console.log("useMounted", window.innerWidth);
+  });
+
   return (
     <div>
       <h1>Hello</h1>
